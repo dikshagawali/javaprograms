add10081ce681c9144259675df64b19bfea18eb0
dikshagawali <dikshagawali321@gmail.com>
2024-01-14 21:14:03 +0530

    initial commit

---
+++ Src/Program1.java
@@ -0,0 +1,19 @@
+package Src;
+
+public class Program1 {
+
+    public static void main(String[] args){
+
+        int i = 3;
+
+        while (i < 9){
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+    }
+
+
+
+}
---
+++ Src/Program2.java
@@ -0,0 +1,16 @@
+package Src;
+
+public class Program2 {
+
+    public static void main(String[] args){
+
+    String s1 = "abc";
+
+    StringBuffer s2 = new StringBuffer(s1);
+        System.out.println(s1.equals(s2));
+
+    }
+
+
+
+}
